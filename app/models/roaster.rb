80ee3c41d211f06295d4c41f71d6245e175ce26b
Owen <ocole161@gmail.com>
2023-02-13 15:21:51 -0700

    Added average_rating methods to coffee_type and roaster models

--- app/models/roaster.rb
+++ app/models/roaster.rb
@@ -1,4 +1,8 @@
 class Roaster < ApplicationRecord
     has_many :coffee_types
     has_many :reviews, through: :coffee_types
+
+    def average_rating
+        self.reviews.average(:rating).to_i
+    end
 end
